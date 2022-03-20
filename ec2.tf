cb3aea5cd46387c49da6153b6d23d548d8cc6fd5
shreyansh2953 <shreyanshnowlkha97@gmail.com>
2022-03-20 21:34:17 +0530

    some changes

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ubuntu" {
     subnet_id = var.my_subnet_id
     iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
     security_groups = [aws_security_group.allow_tls.id]
-    key_name = "mykey"
+    key_name = "mykey-ubuntu"
     
 
      tags = {
